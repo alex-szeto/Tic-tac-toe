380fc113e201b76eedbfb2f33bc8ba0045ca772a
alex.szeto <alex.szeto.a@gmail.com>
2020-03-27 11:01:41 -0400

    Finished

--- index.js
+++ index.js
@@ -7,18 +7,43 @@ $("#boardContainer").on("click", (event) => {
     if(event.target.className != "boardElement" || event.target.innerText.length != 0) return
 
     const selectedSquare = [event.target.id.match(/^\d/)[0], event.target.id.match(/\d$/)[0]]
-    console.log(board)
 
     if(turn == "blue"){
         event.target.innerText = "X"
+        event.target.style.color = "blue"
         board[selectedSquare[0]][selectedSquare[1]] = "X"
     } else if(turn == "red"){
         event.target.innerText = "O"
+        event.target.style.color = "red"
         board[selectedSquare[0]][selectedSquare[1]] = "O"
     }
     
-    updateTurn(turn)
+    if(validateBoard(selectedSquare)){
+        alert(`Game over! ${turn.charAt(0).toLocaleUpperCase() + turn.slice(1,turn.length)} has won!`)
+        currentGame = false
+        $("#turn").text("Undefined")
+        $("#turn").css("color", "")
+        return
+    } else {
+        updateTurn(turn)
+    }
 })
+
+function validateBoard(curr){
+    let move
+    turn === "blue" ? move = "X" : move = "O"
+
+    let [horizontal, vertical] = [true, true]
+    let diag = (move == board[0][0] && move==board[1][1] && move == board[2][2]) || (move == board[0][2] && move == board[1][1] && move == board[2][0])
+
+    for(let i = 0; board.length > i; i++){
+        if(board[i][curr[1]] != move) vertical = false
+        if(board[curr[0]][i] != move) horizontal = false
+    }
+
+    return horizontal || vertical || diag
+
+}
 
 function updateTurn(prev="nil"){
     switch(prev){
@@ -27,12 +52,12 @@ function updateTurn(prev="nil"){
         break;
         case "blue":
             turn = "red"
-            $("#turn").text("Red")
+            $("#turn").text("Red (O)")
             $("#turn").css("color", "red")
         break;
         case "red":
             turn = "blue"
-            $("#turn").text("Blue")
+            $("#turn").text("Blue (X)")
             $("#turn").css("color", "blue")
         break;
     }
@@ -42,6 +67,12 @@ $("#startGame").on("click", () =>{
     if(currentGame == false){
         board = [...Array(3)].map(e => Array(3).fill("."))
 
+        for(let i = 0; board.length > i; i++){
+            for(let j = 0; board[i].length > j ; j++){ 
+                $("#"+i+"_"+j).text("")
+            }
+        }
+
         updateTurn()
         $("#startGame").text("End Game")
         currentGame = true
@@ -49,5 +80,7 @@ $("#startGame").on("click", () =>{
         $("#startGame").text("Start Game")
         currentGame = false
         turn = "undefined"
+        $("#turn").text("Undefined")
+        $("#turn").css("color", "")
     }
 })
